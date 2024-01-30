094a30030f8f0e23c460f3c3a366b101303c9fe7
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-01-30 15:47:14 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// include all necessary header and source files 
+#include "code.h"
+#include "code.cpp"
+#include "response.h"
+#include "response.cpp"
+#include "mastermind.h"
+#include "mastermind.cpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{}
