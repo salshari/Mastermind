56722e8f0d6301c0c3b011dee6f04c1faacbde6f
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-01-30 21:26:53 +0000

    Create the header file for mastermind class in mastermind.h

---
+++ mastermind.h
@@ -0,0 +1,34 @@
+#include "response.h"
+#include "response.cpp"
+#include "code.h"
+#include "code.cpp"
+
+using namespace std;
+
+class mastermind{
+    public:
+
+    // code object as data member 
+    code x;
+
+    // constructor
+    mastermind();
+
+    // function to print code
+    void printCode() const;
+
+    // function that reads guess from keyboard and returns a code object that represents the guess
+    vector<int> humanGuess();
+
+    // function that is passed one code (a guess code), and returns a response
+    response getResponse();
+
+    // function that is passed a response and returns True if the resposne indicates that the board has been solved
+    bool isSolved();
+
+    /*
+    function that initializes a random code, prints it to the screen, and then iteratively gets a guess from the 
+    user and prints the response until either the codemaker (computer) or the codebreaker has won
+    */
+    void playGame();
+}
