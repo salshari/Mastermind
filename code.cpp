74c5cb1cb9359ddeea9df0af5e5398a6203d5985
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-02-03 18:13:11 +0000

    Improve readability of code.cpp file and add comments

--- code.cpp
+++ code.cpp
@@ -6,41 +6,67 @@ private:
     int m;
     vector<int> secretCode;
 public:
+
+    // default constructor
+    Code (){}
+
     Code(int length, int range): n(length), m(range) {}
 
+    // full function definition for the randomlyInitialize function
     void randomlyInitialize(){
-        secretCode.clear();
-        for (int i=0;i<n;i++){
-            secretCode.push_back(rand()%m);
 
+        // clear any previously saved secretCode
+        secretCode.clear();
+
+        for (int i = 0; i < n; i++)
+        {
+            secretCode.push_back(rand()%m);
         }
     }
 
-    int checkCorrect(const vector<int>& guess){
-        if (guess.size() != n){
+    // full function defintion for the checkCorrect function
+    int checkCorrect(const vector<int>& guess)
+    {
+        // initialize the number of correctDigits to 0
+        int correctDigits = 0;
+
+        // if the guess is not the correct length, the user has given an invalid argument
+        if (guess.size() != n)
+        {
             throw invalid_argument("Invalid guess length");
         }
-        int correctDigits = 0;
-        for (int i=0; i<n; i++){
-            if (guess[i]==secretCode[i]){
-                correctDigits +=1;
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (guess[i] == secretCode[i])
+            {
+                correctDigits += 1;
             }
         }
+
         return correctDigits;
     }
 
-    int checkIncorrect(const vector<int>&guess){
-        if (guess.size() !=n){
+    // full function definiton for the checkIncorrect function
+    int checkIncorrect(const vector<int>&guess)
+    {
+        int incorrectDigits = 0;
+
+        if (guess.size() != n)
+        {
             throw invalid_argument("Invalid guess length");
         }
-        int incorrectDigits = 0;
+        
         vector<bool> secretUsed(n,false);
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if(!secretUsed[j]&& guess[i] == secretCode[j]){
-                    incorrectDigits +=1;
-                    secretUsed[j]=true;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(!secretUsed[j] && guess[i] == secretCode[j])
+                {
+                    incorrectDigits += 1;
+                    secretUsed[j] = true;
                     break;
                 }
             }
