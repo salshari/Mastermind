4c951b50082d76315ec68b2ef725bffb79701925
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-02-03 04:20:00 +0000

    Fill in code.h file with code and comments

--- code.h
+++ code.h
@@ -2,3 +2,36 @@
 #include <vector>
 using namespace std;
 
+class Code
+{
+    private:
+
+    // length of code sequence
+    int n;
+
+    // range of code sequence is [0,m-1]
+    int m;
+
+    vector<int> secretCode;
+    
+    public:
+
+    // constructor 
+    Code();
+
+    // function to initialize the code randomly
+    void randomlyInitialize();
+
+    /*
+    function with guess as a parameter (another code object) that returns the 
+    number of correct digits in the correct location 
+    */
+    int checkCorrect();
+
+    /*
+    function with guess as a parameter (another code object) that returns the 
+    number of correct digits in the incorrect location.
+    No digit in the guess or the code is counted more than once
+    */
+    int checkIncorrect();
+};
