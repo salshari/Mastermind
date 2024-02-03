26de606e2a5fd6c5abaac4417775987927c8f21a
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-02-03 04:20:51 +0000

    Fix typos in mastermind.h file

--- mastermind.h
+++ mastermind.h
@@ -7,7 +7,7 @@ class mastermind{
     public:
 
     // code object as data member 
-    code x;
+    Code x;
 
     // constructor
     mastermind();
@@ -19,14 +19,14 @@ class mastermind{
     vector<int> humanGuess();
 
     // function that is passed one code (a guess code), and returns a response
-    response getResponse();
+    Response getResponse();
 
     // function that is passed a response and returns True if the resposne indicates that the board has been solved
-    bool isSolved();
+    bool isSolved(Response y);
 
     /*
     function that initializes a random code, prints it to the screen, and then iteratively gets a guess from the 
     user and prints the response until either the codemaker (computer) or the codebreaker has won
     */
     void playGame();
-}
+};
