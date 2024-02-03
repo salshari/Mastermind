842fb73477ca7ede86702d8820e592c5052412f5
Shyma Alshari <150717904+salshari@users.noreply.github.com>
2024-02-03 19:57:41 +0000

    Edit code.h file

--- response.h
+++ response.h
@@ -1,2 +1,28 @@
 #include "code.h"
 #include "code.cpp"
+
+#include <iostream>
+
+using namespace std;
+
+class Response
+{
+    private:
+        int correctDigits;
+        int incorrectDigits;
+    public:
+        Response(int correct, int incorrect);
+
+        int getCorrectDigits() const ;
+
+        int getIncorrectDigits() const;
+
+        void setCorrectDigits(int correct);
+
+        void setIncorrectDigits(int incorrect);
+
+        bool operator ==(const Response& other)const;
+
+        friend ostream& operator<<(ostream&out,Response&A);
+
+    };
